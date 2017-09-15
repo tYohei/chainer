b719e20420211deb71062141ea6f398504c412ed
Owner <owner@MR15-253.local>
2017-09-15 16:59:42 +0900

    Fix flake8

--- chainer/training/extensions/snapshots/__init__.py
+++ chainer/training/extensions/snapshots/__init__.py
@@ -6,9 +6,9 @@ from chainer.training.extensions.snapshots import writer  # NOQA
 
 # import class and function
 from chainer.training.extensions.snapshots.condition import Always  # NOQA
-from chainer.training.extensions.snapshots.handler import SerializerHandler  # NOQA
 from chainer.training.extensions.snapshots.handler import save_hdf5  # NOQA
 from chainer.training.extensions.snapshots.handler import save_npz  # NOQA
+from chainer.training.extensions.snapshots.handler import SerializerHandler  # NOQA
 from chainer.training.extensions.snapshots.writer import ProcessQueueWriter  # NOQA
 from chainer.training.extensions.snapshots.writer import ProcessWriter  # NOQA
 from chainer.training.extensions.snapshots.writer import QueueWriter  # NOQA
